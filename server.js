76ac63d1b824d7c161928fd703eda4f2c671dabf
Arpad Vas <avas@corp-ad.credorax.com>
2017-01-16 16:39:37 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var express = require('express');
+var app = express();
+
+app.listen(process.env.PORT || 3000, function() {
+	console.log('The server is running on port 3000!');
+});
