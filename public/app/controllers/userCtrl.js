f8f5564622bd40e32e4ad7b0790b952fba746889
Arpad Vas <vas.arpad@gmail.com>
2017-01-17 23:23:17 +0100

    registration page

--- public/app/controllers/userCtrl.js
+++ public/app/controllers/userCtrl.js
@@ -1,11 +1,25 @@
 angular.module('userControllers', [])
 
-.controller('regCtrl', function($http) {
+.controller('regCtrl', function($http, $location) {
+
+	var app = this;
 	
 	this.regUser = function(regData) {
+		app.loading = true;
+		app.errorMsg = false;
 		console.log('form submitted');
-		console.log(this.regData);
-		$http.post('/api/users', this.regData);
+		$http.post('/api/users', this.regData).then(function(data) {
+			console.log(data.data.success);
+			console.log(data.data.message);
+			if(data.data.success) {
+				app.loading = false;
+				app.successMsg = data.data.message;
+				$location.path('/');
+			} else {
+				app.loading = false;
+				app.errorMsg = data.data.message;
+			}
+		});
 	};
 
 });
