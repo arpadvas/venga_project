e0ab60c13ccd881d3cfeea78c2104ea8f6e2a20d
Arpad Vas <avas@corp-ad.credorax.com>
2017-01-18 17:05:19 +0100

    reg page

--- public/app/controllers/userCtrl.js
+++ public/app/controllers/userCtrl.js
@@ -1,20 +1,21 @@
 angular.module('userControllers', [])
 
-.controller('regCtrl', function($http, $location) {
+.controller('regCtrl', function($http, $location, $timeout) {
 
 	var app = this;
 	
 	this.regUser = function(regData) {
 		app.loading = true;
 		app.errorMsg = false;
-		console.log('form submitted');
 		$http.post('/api/users', this.regData).then(function(data) {
 			console.log(data.data.success);
 			console.log(data.data.message);
 			if(data.data.success) {
 				app.loading = false;
-				app.successMsg = data.data.message;
-				$location.path('/');
+				app.successMsg = data.data.message + ' Redirecting...';
+				$timeout(function() {
+					$location.path('/');
+				}, 2000);
 			} else {
 				app.loading = false;
 				app.errorMsg = data.data.message;
