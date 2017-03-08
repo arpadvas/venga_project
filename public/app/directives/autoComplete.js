7e3047a4628391b378dcfdf9637b951ac0120b38
Arpad Vas <vas.arpad@gmail.com>
2017-03-08 22:52:39 +0100

    autocomplete

--- public/app/directives/autoComplete.js
+++ public/app/directives/autoComplete.js
@@ -4,10 +4,20 @@ angular.module('autocompleteDirective', [])
 
 	return {
 		restrict: 'E',
-		templateUrl: 'app/views/template.html'
-		// scope: {
-			
-		// }
+		templateUrl: 'app/views/template.html',
+		scope: {
+			hide: '=',
+			datasource: '=',
+			action: '&'
+		},
+		controller: function($scope) {
+			this.log = function() {
+				console.log('hi');
+			};
+		},
+		link: function(scope, element, attr, ctrl) {
+			ctrl.log();
+		}
 	};
 
 });
